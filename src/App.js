dc6b317822a1eae176d10722b1b8e43ff80843a1
Elton Zheng <elton.zheng.sh@gmail.com>
2021-03-03 17:46:03 +0800

    Feat: Add React router support.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { renderRoutes } from 'react-router-config';
+import { HashRouter } from 'react-router-dom';
+import routes from './routes/index';
+
 function App() {
-  return <div>Hello World</div>;
+  return <HashRouter>{renderRoutes(routes)}</HashRouter>;
 }
 
 export default App;
